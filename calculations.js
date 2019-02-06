ca9274b8846d58c0560eb1d0cf37c22ff89c1b31
Ilya Libin <ilya@epsagon.com>
2019-02-06 18:12:56 +0200

    calculation working

--- calculations.js
+++ calculations.js
@@ -1,11 +1,49 @@
 'use strict';
 var AWS = require('aws-sdk');
 AWS.config.update({region: 'eu-central-1'});
+const INTERVAL = 1000 * 60 * 10 // 10 minutes
 
 module.exports.average = async (event, context) => {
-  console.log(average)
+  let doc = event.Records[0].dynamodb.NewImage;
+  doc = AWS.DynamoDB.Converter.unmarshall(doc);
+
+  var db = new AWS.DynamoDB.DocumentClient();
+  const moreThenTimestamp = doc.timestamp - (doc.timestamp % INTERVAL);
+
+  var scanQuery = {
+    TableName: 'datapoints',
+    FilterExpression: 'beer = :beer_name and #stamp between :more_then_timestamp and :less_then_timestamp',
+    ExpressionAttributeValues: {
+      ':beer_name': doc.beer,
+      ':more_then_timestamp': moreThenTimestamp,
+      ':less_then_timestamp': doc.timestamp + 1
+    },
+    ExpressionAttributeNames: {
+      "#stamp": "timestamp"
+    }
+  };
+
+  var response = await db.scan(scanQuery).promise();
+  if (!response.Count) return { statusCode: 201, body: '' }
+
+  const { Items } = response;
+
+  var updateQuery = {
+    TableName: 'averages',
+    UpdateExpression: "set temp = :temp and gravity = :gravity and datapoints = :datapoints",
+    Item: {
+      beer: doc.beer,
+      timestamp: moreThenTimestamp,
+      datapoints: response.Count,
+      gravity: Items.reduce((p, c, i) => p + (c.gravity - p) / (i + 1), 0),
+      temp: Items.reduce((p, c, i) => p + (c.temp - p) / (i + 1), 0),
+    }
+  }
+
+  response = await db.put(updateQuery).promise();
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Success' }),
+    body: JSON.stringify({ message: response }),
   };
 };
