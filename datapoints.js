a5f9781bec4bfba14bbe1f70af3e8f4d991e5950
Ilya Libin <ilya@epsagon.com>
2019-03-14 17:28:23 +0200

    asdf

--- datapoints.js
+++ datapoints.js
@@ -28,6 +28,26 @@ module.exports.create = async ({ body }, context) => {
 };
 
 module.exports.index = async (event, context) => {
+  const db = new AWS.DynamoDB.DocumentClient();
 
+  var params = {
+    TableName: 'averages'
+  };
+
+  try {
+    const data = await db.scan(params).promise()
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ data }),
+    };
+  } catch(e) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Error',
+        input: e,
+      }),
+    };
+  }
 };
 
